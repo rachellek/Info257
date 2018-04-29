c4895dfb53dc600aa4e9b0c96f4a928d25e9f77f
Rachelle Kresch <rachelle@berkeley.edu>
2018-04-28 21:47:17 -0700

    updates

--- backend.py
+++ backend.py
@@ -5,21 +5,22 @@ import pprint
 
 #Given a new insurance claim, determine whether to increase a customer's premium
 def premium_change(db, CustomerID):
-    claims = db.query("Select ClaimID, Severity from claims WHERE CustomerID==" + CustomerID)
-    pprint.pprint("Customer claims {}".format(claims))
+    claims = list(db.query("Select * from claims WHERE CustomerID==" + CustomerID))
 
-    New_RiskScore = (sum(1 if claims ==1 else 0 for claim in claims) *.2 + \
-    sum(1 if claims ==2 else 0 for claim in claims) *.4 + \
-    sum(1 if claims ==3 else 0 for claim in claims) *.8)
+    New_RiskScore = (sum(1 if int(claim['Severity']) == 1 else 0 for claim in claims) *1.2 + \
+    sum(1 if int(claim['Severity']) == 2 else 0 for claim in claims) *1.4 + \
+    sum(1 if int(claim['Severity']) == 3 else 0 for claim in claims) *1.8)
     print("Customer new Risk Score {}".format(New_RiskScore))
 
-    RiskScore = db.query("Select RiskScore from customer WHERE CustomerID==" + CustomerID)
+    RiskScore = int(list(db.query("Select RiskScore from customer WHERE CustomerID==" + CustomerID))[0]['RiskScore'])
     print("Customer original Risk Score {}".format(RiskScore))
 
     if New_RiskScore > (RiskScore * 1.5):
         print("The customer's premium has increased")
+        return (True, New_RiskScore)
     else:
         print("The customer's premium has not changed")
+        return (False, RiskScore)
 
 if __name__ == "__main__":
     db = dataset.connect('sqlite:///AutoInsurace.db', row_type=stuf)
